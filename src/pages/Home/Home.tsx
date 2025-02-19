946ef6ab501b9ab940cc3f4cd02a5d92b3a1afe1
Anton <antonpaper500@gmail.com>
2025-02-19 11:58:19 +0300

    updated

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,7 +12,8 @@ import { YEARS_2015_2022 } from "../../years";
 gsap.registerPlugin(MotionPathPlugin);
 
 const Home: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const [currentYear, setCurrentYear] = useState<number>(5);
+
   const itemsRef = useRef<HTMLDivElement[]>([]);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
@@ -22,7 +23,10 @@ const Home: React.FC = () => {
 
   const step = 1 / lengthOfItems;
   const wrapProgress = gsap.utils.wrap(0, 1);
+  const wrapTracker = gsap.utils.wrap(0, lengthOfItems);
   const snap = gsap.utils.snap(step);
+
+  let tracker = { item: 0 };
 
   useGSAP(
     () => {
@@ -36,7 +40,7 @@ const Home: React.FC = () => {
           path: circlePath,
           align: circlePath,
           alignOrigin: [0.5, 0.5],
-          end: (i: number) => i / itemsRef.current.length,
+          end: (i: number) => i / lengthOfItems,
         },
         scale: 1,
       });
@@ -60,24 +64,24 @@ const Home: React.FC = () => {
         },
         0
       );
+
+      tl.current.to(
+        tracker,
+        {
+          item: lengthOfItems,
+          duration: 1,
+          ease: "none",
+          modifiers: {
+            item(value: number) {
+              const newItem =  wrapTracker(lengthOfItems - 1 - Math.round(value))
+              setCurrentYear(newItem)
+              return newItem;
+            },
+          },
+        },
+        0
+      );
     },
-
-    // document.querySelector('.item.active').classList.remove('active');
-    // items[activeItem].classList.add('active');
-
-    // var diff = current - i;
-
-    // if (Math.abs(diff) < numItems / 2) {
-    //   moveWheel(diff * itemStep);
-    // } else {
-    //   var amt = numItems - Math.abs(diff);
-
-    //   if (current > i) {
-    //     moveWheel(amt * -itemStep);
-    //   } else {
-    //     moveWheel(amt * itemStep);
-    //   }
-    // }
 
     { scope: wrapperRef }
   );
@@ -103,8 +107,38 @@ const Home: React.FC = () => {
       });
     }
   };
+    function moveWheel(amount: any) {
+    gsap.to(tl.current, {
+      progress: snap(tl.current.progress() + amount),
+      modifiers: {
+        progress: wrapProgress
+      }
+    });
+  }
 
-  console.log(YEARS_2015_2022);
+  const handleMove = (index: number) => {
+    if (index === currentYear) return;
+  
+    let diff = currentYear - index;
+    let newProgress;
+
+    if (Math.abs(diff) < lengthOfItems / 2) {
+      newProgress = snap(tl.current.progress() + diff * step);
+    } else {
+      let amt = lengthOfItems - Math.abs(diff);
+      newProgress = snap(
+        tl.current.progress() + (currentYear > index ? -amt * step : amt * step)
+      );
+    }
+  
+    gsap.to(tl.current, {
+      progress: newProgress,
+      modifiers: {
+        progress: wrapProgress,
+      },
+    });
+  };
+
 
   return (
     <>
@@ -117,16 +151,27 @@ const Home: React.FC = () => {
 
           {/* Круг с годами */}
           <div className={style.circular__container}>
+            <div className={style.year__container}>
+              <span className={style.year__first}>2015</span>
+              <span className={style.year__last}>2022</span>
+            </div>
             <div className={style.circular__carousel}>
               <div className={style.wrapper} ref={wrapperRef}>
                 <div className={style.items}>
                   {[...Array(lengthOfItems)].map((_, i) => (
                     <div
                       key={i}
+                      onClick={() => {
+                        handleMove(i);
+                      }}
                       className={style.item}
                       ref={(el: any) => el && (itemsRef.current[i] = el)}
                     >
-                      <div className={style.item__content}>{i + 1}</div>
+                      {currentYear === i ? (
+                        <div className={style.item__active}>{i + 1}</div>
+                      ) : (
+                        <div className={style.item__content}>{i + 1}</div>
+                      )}
                     </div>
                   ))}
                 </div>
@@ -145,12 +190,13 @@ const Home: React.FC = () => {
           </div>
 
           <div className={style.actions__container}>
-            <div
-              className={style.buttons__container}
-              style={{ textAlign: "center" }}
-            >
-              <button onClick={handlePrev}>Prev</button>
-              <button onClick={handleNext}>Next</button>
+            <div className={style.buttons__container}>
+              <button className={style.button__prev} onClick={handlePrev}>
+                Prev
+              </button>
+              <button className={style.button__prev} onClick={handleNext}>
+                Next
+              </button>
             </div>
 
             <div className={style.swiper__container}>
@@ -163,8 +209,9 @@ const Home: React.FC = () => {
                 modules={[Pagination]}
                 className="mySwiper"
               >
-                {YEARS_2015_2022.map((e: any, i) => (
+                {YEARS_2015_2022.map((e: any, i: number) => (
                   <SwiperSlide
+                    key={`slide-${i}`}
                     style={{ maxWidth: "320px" }}
                     className={style.swiper__slide}
                   >
@@ -182,3 +229,19 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+// document.querySelector('.item.active').classList.remove('active');
+// items[activeItem].classList.add('active');
+
+// var diff = current - i;
+
+// if (Math.abs(diff) < numItems / 2) {
+//   moveWheel(diff * itemStep);
+// } else {
+//   var amt = numItems - Math.abs(diff);
+
+//   if (current > i) {
+//     moveWheel(amt * -itemStep);
+//   } else {
+//     moveWheel(amt * itemStep);
+//   }
+// }
